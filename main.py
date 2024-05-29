8735e3240575450e5b96994bc743d90f7e2ea652
majeed1419 <124386160+majeed1419@users.noreply.github.com>
2024-05-29 23:32:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-from typing import Optional
-
 from fastapi import FastAPI
+import joblib
+from pydantic import BaseModel
+
+model = joblib.load('Models/knn.joblib')
+scaler = joblib.load('Models/scaler.joblib')
 
 app = FastAPI()
 
+# Define a Pydantic model for input data validation
+class InputFeatures(BaseModel):
+    appearance: int
+    minutes_played: int
+    current_value: int
+    award: int
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def preprocessing(input_features: InputFeatures, scaler):
+    dict_f = {
+        'appearance': input_features.appearance,
+        'minutes played': input_features.minutes_played,
+        'award': input_features.award,
+        'current_value': input_features.current_value,
+    }
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    # Scale the input features using the provided scaler
+    scaled_features = scaler.transform([list(dict_f.values())])
+    return scaled_features
+
+@app.post("/predict")
+async def predict(input_features: InputFeatures):
+    # Call preprocessing function with the scaler
+    data = preprocessing(input_features, scaler)
+    y_pred = model.predict(data)
+    return {"pred": y_pred.tolist()[0]}
